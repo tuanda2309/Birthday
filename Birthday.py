3b82f8334f18463ee9a4e678d99236bc3e23b910
Đoàn Anh Tuấn <doananhtuan77qn@gmail.com>
2024-06-21 16:19:12 +0700

    Add files via upload

---
+++ Birthday.py
@@ -0,0 +1,38 @@
+import time
+from random import randint
+
+for i in range(1, 85):
+    print("")
+
+space = ''
+for i in range(1, 1000):
+    count = randint(1, 120)
+    while count > 0:
+        space += ' '
+        count -= 1
+
+    if i % 10 == 0:
+        print(space + '🎂Happy Birthday!')
+    elif i % 9 == 0:
+        print(space + 'Sinh Nhật Vui Vẻ🎁')
+    elif i % 8 == 0:
+        print(space + '🎊')
+    elif i % 7 == 0:
+        print(space + '🎉')
+    elif i % 6 == 0:
+        print(space + '🎈')
+    elif i % 5 == 0:
+        print(space + 'Happy Birthday!💖')
+    elif i % 4 == 0:
+        print(space + '🎇')
+    elif i % 3 == 0:
+        print(space + 'Happy Birthday🌼')
+    else:
+        print(space + ' ')
+    
+    space = ''
+    time.sleep(0.1)
+
+
+
+
